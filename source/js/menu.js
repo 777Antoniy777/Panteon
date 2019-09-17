17be133b0b988c55c0df65c683fc3254275a8df1
Антон <tat7106@gmail.com>
2019-09-17 22:34:41 +0600

    done button menu

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,25 +1,30 @@
-var main_menu = document.querySelector(".main-nav");
-var button_menu = document.querySelector(".main-nav__button");
-var main_header = document.querySelector(".main-header__wrapper");
+'use strict';
+(function () {
+  var header = document.querySelector(".main-header");
+  var mainNav = document.querySelector(".main-nav__wrapper");
+  var introduction = document.querySelector(".introduction");
+  var menu = header.querySelector(".main-nav");
+  var button = menu.querySelector(".main-nav__button");
 
-main_menu.classList.remove("main-nav--nojs");
+  var height = mainNav.offsetHeight + mainNav.offsetTop;
 
-if (main_menu.classList.contains("main-nav--closed")) {
-  main_header.style.paddingTop = '132px';
-  main_header.style.backgroundPosition = '0 0';
-}
+  menu.classList.remove("main-nav--nojs");
+  introduction .style.marginTop = -height + 'px';
 
-button_menu.addEventListener("click", function() {
+  button.addEventListener("click", function() {
 
-  if (main_menu.classList.contains("main-nav--closed")) {
-    main_menu.classList.remove("main-nav--closed");
-    main_menu.classList.add("main-nav--opened");
-    main_header.style.paddingTop = '79px';
-    main_header.style.backgroundPosition = '0 -53px';
-  } else {
-    main_menu.classList.add("main-nav--closed");
-    main_menu.classList.remove("main-nav--opened");
-    main_header.style.paddingTop = '132px';
-    main_header.style.backgroundPosition = '0 0';
-  }
-});
+    if (menu.classList.contains("main-nav--closed")) {
+
+      menu.classList.remove("main-nav--closed");
+      menu.classList.add("main-nav--opened");
+
+      introduction .style.marginTop = '0px';
+
+    } else {
+      menu.classList.add("main-nav--closed");
+      menu.classList.remove("main-nav--opened");
+
+      introduction .style.marginTop = -height + 'px';
+    }
+  });
+})();
